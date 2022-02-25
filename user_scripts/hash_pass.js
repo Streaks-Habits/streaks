e0fdcd2819e4abc303eb6bb0e7bd7eba85f64c36
Olivier Cartier <me@cestoliv.com>
2022-02-25 17:32:25 +0100

    little improvments

--- user_scripts/hash_pass.js
+++ user_scripts/hash_pass.js
@@ -1,14 +1,11 @@
 const bcrypt = require('bcrypt')
-const path = require('path')
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2 || process.argv.length > 4) {
 	console.log("Usage: node user_scripts/hash_pass.js PASSWORD")
 	process.exit(-1)
 }
 
-var password = process.argv[2]
-
-bcrypt.hash(password, 10, function(err, hash) {
+bcrypt.hash(process.argv[2], 10, function(err, hash) {
 	if (err) throw err
 	console.log(hash)
 })
