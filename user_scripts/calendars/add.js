06fae4f72b98f06d0b8b5cc0d7822d60ad63e450
Olivier Cartier <me@cestoliv.com>
2022-07-23 23:17:18 +0200

    fix: 🚑️ init the user to be able to create  the calendar

--- user_scripts/calendars/add.js
+++ user_scripts/calendars/add.js
@@ -3,7 +3,8 @@ const chalk = require('chalk')
 const readlineSync = require('readline-sync')
 const mongoose = require('mongoose')
 
-const { connectDB, User } = require('../../dist/scripts/database/database')
+const { connectDB } = require('../../dist/scripts/database/database')
+const { User } = require('../../dist/scripts/database/User')
 
 process.stdout.write(`${chalk.blue('streaks')} database => `)
 connectDB().then(() => {
@@ -15,12 +16,19 @@ connectDB().then(() => {
 	var calendar_name = readlineSync.question('Calendar name: ')
 	var user = new User(user_id)
 
-	user.addCalendar(calendar_name).then(calendar => {
-		console.log(`Calendar ${calendar.name} (${calendar.id}) ${chalk.green('added')}`)
-	}).catch(err => {
+	user.dbInit().then(() => {
+		user.addCalendar(calendar_name).then(calendar => {
+			console.log(`Calendar ${calendar.name} (${calendar.id}) ${chalk.green('added')}`)
+		}).catch(err => {
+			console.error(`Error: ${chalk.red(err.message)}`)
+		}).finally(() => {
+			mongoose.disconnect()
+			process.exit(1)
+		})
+	}).catch((err) => {
 		console.error(`Error: ${chalk.red(err)}`)
-	}).finally(() => {
 		mongoose.disconnect()
+		process.exit(1)
 	})
 }).catch((err) => {
 	console.error(chalk.red(err))
